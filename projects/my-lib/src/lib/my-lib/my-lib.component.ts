2378eb87a4a8dceffd8d0c802a39519d81c9a07c
Nguyen Van Huy <huynvph25513@fpt.edu.vn>
2023-11-28 02:37:59 +0700

    update

---
+++ projects/my-lib/src/lib/my-lib/my-lib.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'lib-my-lib',
+  template: '<p>This is MyLibComponent from MyLib Library</p>',
+})
+export class MyLibComponent {
+
+}
